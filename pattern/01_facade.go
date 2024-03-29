3826a931d37f8d84d64050f719c19392ccaa4c08
Nikita Kazakov <kazakov.ni@yandex.ru>
2024-03-29 15:22:48 +0300

    feat: 2 patterns are ready

--- pattern/01_facade.go
+++ pattern/01_facade.go
@@ -1,7 +1,119 @@
 package pattern
 
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+)
+
 /*
 	Реализовать паттерн «фасад».
 Объяснить применимость паттерна, его плюсы и минусы,а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Facade_pattern
 */
+
+// Ответ
+
+/*
+Паттерн создаёт единый интерфейс для отвязывания бизнес-логики от сторонних библиотек и реализации сторонних классов.
+Фасад особенно полезен, если  нужна только часть возможностей внешней сложной системы, он скрывает от клиента ненужный ему функционал.
+
+Pros: ослабляет связь между сложной системой сервиса и клиентом
+Cons: усложняет имплементацию приложения, риск для связывания фасада со сложной логикой подсистем
+*/
+
+func start() {
+	order := Order{
+		Products: []Product{
+			{"iPhone"},
+			{"iWatch"},
+		},
+		BuyerID: "customer_account_id",
+		Amount:  200,
+	}
+
+	manager := PackageManager{}
+	packageId, err := manager.PayAndPreparePackage(order)
+
+	if err != nil {
+		panic("Unable to create package: " + packageId)
+	}
+
+	ots := NewOrderTrackingService(os.Getenv("ORDER_TRACKING_SERVICE_TOKEN"))
+	err = ots.TrackPackage(packageId)
+	if err != nil {
+		panic("Unable to track package: " + packageId)
+	}
+
+}
+
+type PaymentsService struct {
+	AuthToken string
+}
+
+func (ps PaymentsService) Execute(amount int, sender string) error {
+	fmt.Printf("Transact $%d from `%s` to company account ID\n", amount, sender)
+	return nil
+}
+
+func NewPaymentsService(token string) *PaymentsService {
+	return &PaymentsService{token}
+}
+
+type FulfilmentService struct {
+	AuthToken string
+}
+
+func NewFulfilmentService(token string) *FulfilmentService {
+	return &FulfilmentService{token}
+}
+
+func (fs FulfilmentService) CreatePackage(products []Product) string {
+	packageId := strconv.Itoa(rand.Intn(3500-2000) + 2000)
+	fmt.Println("Creating new package with ID:", packageId)
+
+	return packageId
+}
+
+type OrderTrackingService struct {
+	AuthToken string
+}
+
+func NewOrderTrackingService(token string) *OrderTrackingService {
+	return &OrderTrackingService{token}
+}
+
+func (ots OrderTrackingService) TrackPackage(id string) error {
+	// Subscribing via webhooks, for instance
+	fmt.Println("Subscribed to track package with ID:", id)
+	return nil
+}
+
+type Order struct {
+	Products []Product
+	BuyerID  string
+	Amount   int
+}
+
+type Product struct {
+	Name string
+}
+
+type PackageManager struct {
+}
+
+func (sm PackageManager) PayAndPreparePackage(order Order) (string, error) {
+	var err error
+
+	paymentService := NewPaymentsService(os.Getenv("PAYMENT_SERVICE_TOKEN"))
+	err = paymentService.Execute(order.Amount, order.BuyerID)
+	if err != nil {
+		return "", err
+	}
+
+	fulfilmentService := NewFulfilmentService(os.Getenv("FULFILMENT_SERVICE_TOKEN"))
+	packageId := fulfilmentService.CreatePackage(order.Products)
+
+	return packageId, err
+}
--- pattern/02_builder.go
+++ pattern/02_builder.go
@@ -1,7 +1,135 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «строитель».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Builder_pattern
 */
+
+/* Паттерн используется в случаях, когда необходимо создавать класс с нетривиальной структурой,
+   "строитель" изолирует процесс создания экземпляра класса от его представления
+
+Pros: Отдельный класс для последовательного создания сложного класса, отвязывает реализацию конструктора от представления класса
+	  Дает более тонкий контроль над процессом конструирования
+Cons:  Усложнение кодовой базы
+*/
+
+type Vehicle struct {
+	engineType string
+	modelType  string
+	bodyType   string
+}
+
+type VehicleBuilder interface {
+	setEngineType()
+	setModelType()
+	setBodyType()
+	getVehicle() Vehicle
+}
+
+func getBuilder(builderType string) VehicleBuilder {
+	if builderType == "car" {
+		return newCarBuilder()
+	}
+
+	if builderType == "truck" {
+		return newTruckBuilder()
+	}
+	return nil
+}
+
+type CarBuilder struct {
+	engineType string
+	modelType  string
+	bodyType   string
+}
+
+func newCarBuilder() *CarBuilder {
+	return &CarBuilder{}
+}
+
+func (b *CarBuilder) setBodyType() {
+	b.bodyType = "saloon"
+}
+
+func (b *CarBuilder) setEngineType() {
+	b.engineType = "petroleum"
+}
+
+func (b *CarBuilder) setModelType() {
+	b.modelType = "family"
+}
+
+func (b *CarBuilder) getVehicle() Vehicle {
+	return Vehicle{bodyType: b.bodyType,
+		modelType:  b.modelType,
+		engineType: b.engineType,
+	}
+}
+
+type TruckBuilder struct {
+	engineType string
+	modelType  string
+	bodyType   string
+}
+
+func newTruckBuilder() *TruckBuilder {
+	return &TruckBuilder{}
+}
+
+func (b *TruckBuilder) setBodyType() {
+	b.bodyType = "trailer"
+}
+
+func (b *TruckBuilder) setEngineType() {
+	b.engineType = "disel"
+}
+
+func (b *TruckBuilder) setModelType() {
+	b.modelType = "van"
+}
+
+func (b *TruckBuilder) getVehicle() Vehicle {
+	return Vehicle{bodyType: b.bodyType,
+		modelType:  b.modelType,
+		engineType: b.engineType,
+	}
+}
+
+type Director struct {
+	builder VehicleBuilder
+}
+
+func newDirector(b VehicleBuilder) *Director {
+	return &Director{
+		builder: b,
+	}
+}
+
+func (d *Director) setBuilder(b VehicleBuilder) {
+	d.builder = b
+}
+
+func (d *Director) buildVehicle() Vehicle {
+	d.builder.setBodyType()
+	d.builder.setEngineType()
+	d.builder.setModelType()
+	return d.builder.getVehicle()
+}
+
+func main() {
+	carBuilder := getBuilder("saloom")
+	truckBuilder := getBuilder("truck")
+
+	director := newDirector(carBuilder)
+	carVehicle := director.buildVehicle()
+
+	fmt.Printf("Normal House Door Type: %s\n", carVehicle.modelType)
+	fmt.Printf("Normal House Window Type: %s\n", carVehicle.engineType)
+	fmt.Printf("Normal House Num Floor: %d\n", carVehicle.bodyType)
+
+	director.setBuilder(truckBuilder)
+	truckVehicle := director.buildVehicle()
+}
