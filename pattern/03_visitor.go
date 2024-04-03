d2640ed2149b8b4872d1a8462e0836793c5a0769
Nikita Kazakov <kazakov.ni@yandex.ru>
2024-04-03 16:05:27 +0300

    command pattern carried out

--- pattern/03_visitor.go
+++ pattern/03_visitor.go
@@ -1,7 +1,129 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «посетитель».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Visitor_pattern
-*/
+
+
+
+
+Шаблон следует использовать, если:
+
+имеются различные объекты разных классов с разными интерфейсами, но над ними нужно совершать операции, зависящие от конкретных классов;
+нужно выполнить операцию над всеми элементами сложной структуры объекта (например, дерева объектов).
+часто добавляются новые операции над структурой и они загрязняют бизнес-логику.
+
+
+Преимущества:
+
+упрощается добавление новых операций;
+объединение родственных операции в классе Visitor;
+класс Visitor может запоминать в себе какое-то состояние по мере обхода контейнера.
+
+Недостатки:
+
+затруднено добавление новых классов, поскольку нужно обновлять иерархию посетителя и его сыновей.*/
+
+type Shape interface {
+	getType() string
+	accept(Visitor)
+}
+
+type Square struct {
+	side int
+}
+
+func (s *Square) accept(v Visitor) {
+	v.visitForSquare(s)
+}
+
+func (s *Square) getType() string {
+	return "Square"
+}
+
+type Circle struct {
+	radius int
+}
+
+func (c *Circle) accept(v Visitor) {
+	v.visitForCircle(c)
+}
+
+func (c *Circle) getType() string {
+	return "Circle"
+}
+
+type Rectangle struct {
+	l int
+	b int
+}
+
+func (t *Rectangle) accept(v Visitor) {
+	v.visitForrectangle(t)
+}
+
+func (t *Rectangle) getType() string {
+	return "rectangle"
+}
+
+type Visitor interface {
+	visitForSquare(*Square)
+	visitForCircle(*Circle)
+	visitForrectangle(*Rectangle)
+}
+
+type AreaCalculator struct {
+	area int
+}
+
+func (a *AreaCalculator) visitForSquare(s *Square) {
+	// Calculate area for square.
+	// Then assign in to the area instance variable.
+	fmt.Println("Calculating area for square")
+}
+
+func (a *AreaCalculator) visitForCircle(s *Circle) {
+	fmt.Println("Calculating area for circle")
+}
+func (a *AreaCalculator) visitForrectangle(s *Rectangle) {
+	fmt.Println("Calculating area for rectangle")
+}
+
+type MiddleCoordinates struct {
+	x int
+	y int
+}
+
+func (a *MiddleCoordinates) visitForSquare(s *Square) {
+	// Calculate middle point coordinates for square.
+	// Then assign in to the x and y instance variable.
+	fmt.Println("Calculating middle point coordinates for square")
+}
+
+func (a *MiddleCoordinates) visitForCircle(c *Circle) {
+	fmt.Println("Calculating middle point coordinates for circle")
+}
+func (a *MiddleCoordinates) visitForrectangle(t *Rectangle) {
+	fmt.Println("Calculating middle point coordinates for rectangle")
+}
+
+func main() {
+	square := &Square{side: 2}
+	circle := &Circle{radius: 3}
+	rectangle := &Rectangle{l: 2, b: 3}
+
+	areaCalculator := &AreaCalculator{}
+
+	square.accept(areaCalculator)
+	circle.accept(areaCalculator)
+	rectangle.accept(areaCalculator)
+
+	fmt.Println()
+	middleCoordinates := &MiddleCoordinates{}
+	square.accept(middleCoordinates)
+	circle.accept(middleCoordinates)
+	rectangle.accept(middleCoordinates)
+}
--- pattern/04_command.go
+++ pattern/04_command.go
@@ -1,7 +1,91 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «комманда».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
-	https://en.wikipedia.org/wiki/Command_pattern
+Применяется:
+- Когда вы хотите параметризовать объекты выполняемым действием.
+- Когда вы хотите ставить операции в очередь, выполнять их по расписанию или передавать по сети.
+- Когда вам нужна операция отмены.
+
+Плюсы:
+- Убирает прямую зависимость между объектами, вызывающими операции, и объектами, которые их непосредственно выполняют.
+- Позволяет реализовать простую отмену и повтор операций.
+- Позволяет реализовать отложенный запуск операций.
+- Позволяет собирать сложные команды из простых.
+- Реализует принцип открытости/закрытости.
+
+Минус:
+- Усложняет код программы из-за введения множества дополнительных классов.
 */
+
+type Command interface {
+	execute()
+}
+
+type Button struct {
+	command Command
+}
+
+func (b *Button) press() {
+	b.command.execute()
+}
+
+type OnCommand struct {
+	device Device
+}
+
+func (c *OnCommand) execute() {
+	c.device.on()
+}
+
+type OffCommand struct {
+	device Device
+}
+
+func (c *OffCommand) execute() {
+	c.device.off()
+}
+
+type Device interface {
+	on()
+	off()
+}
+
+type Tv struct {
+	isRunning bool
+}
+
+func (t *Tv) on() {
+	t.isRunning = true
+	fmt.Println("Turning tv on")
+}
+
+func (t *Tv) off() {
+	t.isRunning = false
+	fmt.Println("Turning tv off")
+}
+
+func main() {
+	tv := &Tv{}
+
+	onCommand := &OnCommand{
+		device: tv,
+	}
+
+	offCommand := &OffCommand{
+		device: tv,
+	}
+
+	onButton := &Button{
+		command: onCommand,
+	}
+	onButton.press()
+
+	offButton := &Button{
+		command: offCommand,
+	}
+	offButton.press()
+}
