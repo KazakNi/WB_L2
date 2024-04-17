d8163c27148392b3f614ab23c5d4985e13b0b079
Nikita Kazakov <kazakov.ni@yandex.ru>
2024-04-17 16:18:10 +0300

    feat:patterns are complete

--- pattern/07_strategy.go
+++ pattern/07_strategy.go
@@ -1,7 +1,108 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «стратегия».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Strategy_pattern
+
+	Применяется при:
+	- Когда вам нужно использовать разные вариации какого-то алгоритма внутри одного объекта.
+	- Когда вы не хотите обнажать детали реализации алгоритмов для других классов.
+	- Когда различные вариации алгоритмов реализованы в виде развесистого условного оператора. Каждая ветка такого оператора представляет собой вариацию алгоритма.
+
+	Плюсы:
+	- Горячая замена алгоритмов на лету.
+ 	- Изолирует код и данные алгоритмов от остальных классов.
+ 	- Уход от наследования к делегированию.
+ 	- Реализует принцип открытости/закрытости.
+
+	Минусы:
+	- Усложняет программу за счёт дополнительных классов.
+	- Клиент должен знать, в чём состоит разница между стратегиями, чтобы выбрать подходящую.
 */
+
+type EvictionAlgo interface {
+	evict(c *Cache)
+}
+
+type Fifo struct {
+}
+
+func (l *Fifo) evict(c *Cache) {
+	fmt.Println("Evicting by fifo strtegy")
+}
+
+type Lru struct {
+}
+
+func (l *Lru) evict(c *Cache) {
+	fmt.Println("Evicting by lru strtegy")
+}
+
+type Lfu struct {
+}
+
+func (l *Lfu) evict(c *Cache) {
+	fmt.Println("Evicting by lfu strtegy")
+}
+
+type Cache struct {
+	storage      map[string]string
+	evictionAlgo EvictionAlgo
+	capacity     int
+	maxCapacity  int
+}
+
+func initCache(e EvictionAlgo) *Cache {
+	storage := make(map[string]string)
+	return &Cache{
+		storage:      storage,
+		evictionAlgo: e,
+		capacity:     0,
+		maxCapacity:  2,
+	}
+}
+
+func (c *Cache) setEvictionAlgo(e EvictionAlgo) {
+	c.evictionAlgo = e
+}
+
+func (c *Cache) add(key, value string) {
+	if c.capacity == c.maxCapacity {
+		c.evict()
+	}
+	c.capacity++
+	c.storage[key] = value
+}
+
+func (c *Cache) get(key string) {
+	delete(c.storage, key)
+}
+
+func (c *Cache) evict() {
+	c.evictionAlgo.evict(c)
+	c.capacity--
+}
+
+func main() {
+	lfu := &Lfu{}
+	cache := initCache(lfu)
+
+	cache.add("a", "1")
+	cache.add("b", "2")
+
+	cache.add("c", "3")
+
+	lru := &Lru{}
+	cache.setEvictionAlgo(lru)
+
+	cache.add("d", "4")
+
+	fifo := &Fifo{}
+	cache.setEvictionAlgo(fifo)
+
+	cache.add("e", "5")
+
+}
