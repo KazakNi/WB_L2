141e97963ff94d7e74c26293865cfabe51e341e1
Nikita Kazakov <kazakov.ni@yandex.ru>
2024-04-19 19:11:05 +0300

    feat: dev03 is completed

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -1,21 +1,33 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита sort ===
 
 Отсортировать строки (man sort)
 Основное
 
-Поддержать ключи
+# Поддержать ключи
 
 -k — указание колонки для сортировки
 -n — сортировать по числовому значению
 -r — сортировать в обратном порядке
 -u — не выводить повторяющиеся строки
 
-Дополнительное
+# Дополнительное
 
-Поддержать ключи
+# Поддержать ключи
 
 -M — сортировать по названию месяца
 -b — игнорировать хвостовые пробелы
@@ -24,7 +36,201 @@ package main
 
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
+type Params struct {
+	k int
+	n bool
+	r bool
+	u bool
+}
 
 func main() {
+	file := flag.String("file", "example.txt", "file for parsing")
+	k := flag.Int("k", -1, "column sorting")
+	n := flag.Bool("n", false, "numeric sorting")
+	r := flag.Bool("r", false, "reverse sorting")
+	u := flag.Bool("u", false, "unique strings output")
+
+	flag.Parse()
+
+	params := Params{
+		k: *k,
+		n: *n,
+		r: *r,
+		u: *u,
+	}
+
+	result := SortMan(*file, params)
+
+	fmt.Println(result)
 
 }
+
+func SortMan(file string, params Params) [][]string {
+
+	strings, err := readFile(file, params.u)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// n flag with variations
+	if params.n {
+
+		if params.k != -1 && params.r {
+
+			if err := validateCol(strings, params.k); err != nil {
+				log.Fatal(err)
+			}
+			sort.Slice(strings, func(i, j int) bool {
+				val1, err := strconv.Atoi(strings[i][params.k])
+				if err != nil {
+					log.Fatal(err)
+				}
+				val2, err := strconv.Atoi(strings[j][params.k])
+				if err != nil {
+					log.Fatal(err)
+				}
+				return val1 > val2
+			})
+		} else if params.k != -1 {
+			sort.Slice(strings, func(i, j int) bool {
+				val1, err := strconv.Atoi(strings[i][params.k])
+				if err != nil {
+					log.Fatal(err)
+				}
+				val2, err := strconv.Atoi(strings[j][params.k])
+				if err != nil {
+					log.Fatal(err)
+				}
+				return val1 < val2
+			})
+			return strings
+		}
+		colNum := -1
+		for _, strSlice := range strings {
+			// search for numeric column
+			for col, str := range strSlice {
+				if _, err := strconv.Atoi(str); err == nil {
+					colNum = col
+					break
+				}
+			}
+		}
+
+		if colNum == -1 {
+			return strings
+		}
+
+		if params.r {
+			sort.Slice(strings, func(i, j int) bool {
+				val1, _ := strconv.Atoi(strings[i][colNum])
+				val2, _ := strconv.Atoi(strings[j][colNum])
+				return val1 > val2
+			})
+			return strings
+		}
+
+		sort.Slice(strings, func(i, j int) bool {
+			val1, _ := strconv.Atoi(strings[i][colNum])
+			val2, _ := strconv.Atoi(strings[j][colNum])
+			return val1 < val2
+		})
+		return strings
+	}
+
+	if params.k != -1 {
+		if err := validateCol(strings, params.k); err != nil {
+			log.Fatal(err)
+		}
+		if params.r {
+			sort.Slice(strings, func(i, j int) bool {
+
+				if len(strings[i]) == 0 || len(strings[j]) == 0 {
+					return len(strings[i]) != 0
+				}
+				return strings[i][params.k] > strings[j][params.k]
+			})
+			return strings
+		} else {
+
+			sort.Slice(strings, func(i, j int) bool {
+
+				if len(strings[i]) == 0 || len(strings[j]) == 0 {
+					return len(strings[i]) != 0
+				}
+				return strings[i][params.k] < strings[j][params.k]
+			})
+			return strings
+		}
+	}
+
+	if params.r {
+		sort.Slice(strings, func(i, j int) bool {
+
+			if len(strings[i]) == 0 || len(strings[j]) == 0 {
+				return len(strings[i]) != 0
+			}
+			return strings[i][0] > strings[j][0]
+		})
+
+		return strings
+
+	}
+	sort.Slice(strings, func(i, j int) bool {
+
+		if len(strings[i]) == 0 || len(strings[j]) == 0 {
+			return len(strings[i]) != 0
+		}
+		return strings[i][0] < strings[j][0]
+	})
+
+	return strings
+}
+
+func readFile(file string, u bool) ([][]string, error) {
+	var res [][]string
+
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	if u {
+		var d = make(map[string]bool)
+		for scanner.Scan() {
+
+			line := scanner.Text()
+			if _, ok := d[line]; !ok {
+				res = append(res, strings.Split(line, " "))
+				d[line] = true
+			}
+		}
+	} else {
+
+		for scanner.Scan() {
+			res = append(res, strings.Split(scanner.Text(), " "))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+		return [][]string{}, err
+	}
+
+	return res, nil
+}
+
+func validateCol(strings [][]string, col int) error {
+	for _, slices := range strings {
+		for _, slice := range slices {
+			if len(slice)-1 <= col {
+				return ErrColNum
+			}
+		}
+	}
+	return nil
+}
+
+var ErrColNum = errors.New("column exceeds len of slice")
--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSorting(t *testing.T) {
+	type test struct {
+		params Params
+		file   string
+		res    [][]string
+		err    error
+	}
+
+	tests := []test{
+		{file: "test.txt", res: [][]string{
+			{"ahahaha", "crazy", "test"},
+			{"bimbaza", "bombata"},
+			{"lalalala", "alas"},
+			{"lalalala", "alas"},
+			{"lol", "kek", "cheburek"},
+		},
+			err: nil, params: Params{}},
+		{file: "test.txt", res: [][]string{
+			{"lol", "kek", "cheburek"},
+			{"lalalala", "alas"},
+			{"lalalala", "alas"},
+			{"bimbaza", "bombata"},
+			{"ahahaha", "crazy", "test"},
+		},
+			err: nil, params: Params{r: true}},
+		{file: "test.txt", res: [][]string{
+			{"lalalala", "alas"},
+			{"bimbaza", "bombata"},
+			{"ahahaha", "crazy", "test"},
+			{"lol", "kek", "cheburek"},
+		},
+			err: nil, params: Params{u: true, k: 1}},
+	}
+
+	for _, tc := range tests {
+		got := SortMan(tc.file, tc.params)
+		assert.ElementsMatch(t, got, tc.res)
+	}
+}
