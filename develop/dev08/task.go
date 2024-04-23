2248298014c6d6b62cbdc6ba36c85ac4fa8972d1
Nikita Kazakov <kazakov.ni@yandex.ru>
2024-04-23 22:42:37 +0300

    feat: dev08 has been completed

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 /*
 === Взаимодействие с ОС ===
 
@@ -9,11 +17,20 @@ package main
 поддержать fork/exec команды
 конвеер на пайпах
 
-Реализовать утилиту netcat (nc) клиент
+Реализовать утилиту nc (nc) клиент
 принимать данные из stdin и отправлять в соединение (tcp/udp)
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 
+	var s string
+	fmt.Fscan(reader, &s)
+
+	if strings.Split(s, " ")[0] == "nc" {
+		App()
+	}
+	out := exec.Command("bash", "-c", s)
+	fmt.Println(out)
 }
