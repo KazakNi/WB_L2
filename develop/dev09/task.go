32ea3cf07650b3e3e532c6e0575cee3f625fb534
Nikita Kazakov <kazakov.ni@yandex.ru>
2024-04-24 16:22:01 +0300

    feat:dev09 has been completed

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
 /*
 === Утилита wget ===
 
@@ -9,5 +20,62 @@ package main
 */
 
 func main() {
+	pUrl := flag.String("url", "", "URL to be processed")
+	flag.Parse()
+	url := *pUrl
+	if url == "" {
+		fmt.Fprintf(os.Stderr, "Error: empty URL!\n")
+		return
+	}
 
+	c := colly.NewCollector(colly.AllowedDomains(strings.Split(*pUrl, "//")[1]))
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "1 Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148")
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		filename := path.Base(r.Request.URL.String())
+		url := r.Ctx.Get("url")
+
+		fmt.Println("Checking if " + filename + " exists ...")
+		if _, err := os.Stat(filename); !os.IsNotExist(err) {
+			fmt.Println(filename + " already exists!")
+			return
+		} else {
+			f, err := os.Create(filename)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			defer f.Close()
+
+			if err = r.Save(filename); err != nil {
+				log.Fatal(err)
+			} else {
+
+				fmt.Println("Downloading ", url, " to ", filename)
+				fmt.Printf("filename:%s url:%s", filename, url)
+				fmt.Println(filename + " saved!")
+				log.Printf("[R] %#v", r.Headers)
+			}
+
+		}
+
+	})
+
+	// Start scraping
+	c.Visit(*pUrl)
 }
