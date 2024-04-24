df62fc31783fd44e3dcb9bedb1e0f68357ab5f7e
Nikita Kazakov <kazakov.ni@yandex.ru>
2024-04-24 18:24:05 +0300

    feat:dev10 is completed

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -17,6 +17,36 @@ go-telnet --timeout=3s 1.1.1.1 123
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/reiver/go-telnet"
+)
+
 func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	host := flag.String("host", "", "host for connection")
+	port := flag.String("port", "", "port for connection")
+
+	flag.Parse()
+
+	var caller telnet.Caller = telnet.StandardCaller
+
+	telnet.DialToAndCall(fmt.Sprintf("%s:%s", *host, *port), caller)
+
+	for {
+		select {
+		case <-c:
+			os.Exit(0)
+		case <-time.After(time.Second * 10):
+			fmt.Println("timeout!")
+			os.Exit(0)
+		}
+	}
 
 }
